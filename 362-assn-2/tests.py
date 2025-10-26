822d9993c853606752bb2cf45a7868d79a5d8b90
Rob T <thomr@oregonstate.edu>
2025-10-26 17:11:10 -0600

    Updated 4 tests

---
+++ 362-assn-2/tests.py
@@ -0,0 +1,48 @@
+import unittest
+from contrived_func import contrived_func
+
+
+class TestCase(unittest.TestCase):
+    def test1(self):
+        """Explanation"""
+        contrived_func(3)
+
+    def test2(self):
+        """Explanation"""
+        contrived_func(40)
+    
+    def test3(self):
+        """Explanation"""
+        contrived_func(1)
+    
+    def test4(self):
+        """Explanation"""
+        contrived_func(10)
+   
+    def test4(self):
+        """Explanation"""
+        contrived_func(10)
+   
+    def test4(self):
+        """Explanation"""
+        contrived_func(10)
+   
+    def test4(self):
+        """Explanation"""
+        contrived_func(10)
+   
+    def test4(self):
+        """Explanation"""
+        contrived_func(10)
+   
+    def test4(self):
+        """Explanation"""
+        contrived_func(10)
+   
+    def test4(self):
+        """Explanation"""
+        contrived_func(10)
+
+
+if __name__ == "__main__":
+    unittest.main()
