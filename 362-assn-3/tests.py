b7dea6c47349e930a2cd772fa86a9d92f9a93151
Rob T <thomr@oregonstate.edu>
2025-11-03 18:46:52 -0700

    All tests, fixed unnecessary

--- 362-assn-3/tests.py
+++ 362-assn-3/tests.py
@@ -2,22 +2,34 @@ import unittest
 import random
 from credit_card_validator import credit_card_validator
 
+
 class TestCase(unittest.TestCase):
     def test1(self):
-        tests_to_generate = 200000
-        for i in range(tests_to_generate):
-            expected = True
-        
-            # Generate random lengths between 0-25 digits
-            length = random.randint(0, 26)
+        tests_to_generate = 800000
+        for _ in range(tests_to_generate):
             credit_card_number = ""
-            for i in range(length):
-                credit_card_number += str(random.randint(0,9))
-            
-            with self.subTest():
-                result = credit_card_validator(credit_card_number)
-                msg = 'Failure: {} should be {}'.format(credit_card_number, expected)
-                self.assertEqual(expected, result, msg)
+            odds = random.randint(0, 3)
+
+            # Visa prefix
+            if odds == 1:
+                credit_card_number += str(random.randint(3, 5))
+            # Mastercard prefix
+            if odds == 2:
+                if random.choice([True, False]):
+                    prefix = str(random.randint(50, 56))
+                else:
+                    prefix = str(random.randint(2220, 2721))
+                credit_card_number += prefix
+            # AMEX prefix
+            if odds == 3:
+                credit_card_number += random.choice(
+                    ["33", "34", "35", "36", "37", "38"])
+            # Generate random lengths between 0-18 digits
+            length = random.randint(0, 18)
+
+            for _ in range(length):
+                credit_card_number += str(random.randint(0, 9))
+            credit_card_validator(credit_card_number)
 
 
 if __name__ == "__main__":
